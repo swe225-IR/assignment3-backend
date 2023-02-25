89f83e145ae3006c4a105bc90c3020bbd80efaee
lori <11811407@mail.sustech.edu.cn>
2023-02-24 19:11:56 -0800

    auto commit

--- assignment3_backend/settings.py
+++ assignment3_backend/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+}
--- bckend/views.py
+++ bckend/views.py
@@ -4,7 +4,7 @@ from django.core.handlers.wsgi import WSGIRequest
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 
-from bckend.common.UniResponse import UniResponse, UniResponseEncoder, Result
+from bckend.model.UniResponse import UniResponse, UniResponseEncoder, Result
 
 
 # /api_v2/query?q=xxxx
@@ -12,14 +12,85 @@ from bckend.common.UniResponse import UniResponse, UniResponseEncoder, Result
 def get_results(request: WSGIRequest):
     results = [Result("1", 0.545, {"url": "http://www.a.b.c/path/t"}),
                Result("2", 0.643, {"url": "http://www.a.b.c/path/to"}),
-               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"})]
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+               Result("3", 0.342, {"url": "http://www.a.b.c/path/toc"}),
+
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+               Result("4", 0.2312, {"url": "http://www.a.b.c/4/4/4"}),
+
+
+               ]
     status_code = 200
     query_string = request.GET.get("q")
-    print(query_string)
+    # can set up a cache to store the previous searches (LRU)
+    offset = int(request.GET.get("offset"))
+    limit = int(request.GET.get("limit"))
+    # tokenizer -> stemming ->
     start = time.time()
+    time.sleep(0.2)
     # search results
     # results.append()
     q = time.time() - start
     t = len(results)
-    res = UniResponse({'query_time': q, 'total_number': t, "hits": results})
+    res = UniResponse({'query_time': q, 'total_number': t, "hits": results[offset:offset+limit]})
     return JsonResponse(UniResponseEncoder().encode(res), safe=False)
